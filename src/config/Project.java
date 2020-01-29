1c51d32cfca0f68c81490351d627bd5cc54006a7
MohamedElWaghf <57914752+MohamedElWaghf@users.noreply.github.com>
2020-01-29 19:42:34 +0100

    Update Project.java

--- src/config/Project.java
+++ src/config/Project.java
@@ -2,13 +2,13 @@ package config;
 
 public class Project {
 	public final static String PATH = "";
-	public final static String[] HOSTS = {"localhost", "localhost", "localhost", "localhost", "localhost"}; 
+	public final static String[] HOSTS = {"localhost","localhost"}; 
 	public final static Integer NBHOSTS = HOSTS.length;
     //public final static Integer[] PORTS = {2015, 2016, 2017, 2018, 2019};
-	public final static Integer[] PORTS = {2015, 2016, 2017, 2018, 2019};
+	public final static int[] PORTS = {2020,2021};
 	public static final String DATA = "/data/";
-    public static long Taillefrag = 3100000;
+    public static long Taillefrag = 128000000;
     public static final int Portinit = 2020;
-    public static int Nbmachines = 5;
-    public static int Taillesousfrag = 300;
+    public static int Nbmachines = HOSTS.length;
+    public static int Taillesousfrag = 1000000;
 }
